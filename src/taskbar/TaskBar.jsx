2b5d5ad8ee8b34c41ec84e2dd431302e80e2c457
franjunoy <franjunoy@hotmail.com>
2023-12-20 10:25:12 -0300

    taskbar

--- src/taskbar/TaskBar.jsx
+++ src/taskbar/TaskBar.jsx
@@ -1,35 +1,46 @@
-import styles from './TaskBar.module.css';
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+
 // eslint-disable-next-line react/prop-types
 const TaskBar = ({ isVisible, toggleVisibility }) => {
   return (
-    <div className={`${styles.taskbar} ${!isVisible && styles.hidden}`}>
+    <View style={styles.row}>
       {isVisible && (
         <>
-          <button onClick={toggleVisibility} className={styles.taskbarButton}>
-            X
-          </button>
-          <button className={styles.taskbarButton}>
-            <h3>Mi Factura</h3>
-          </button>
-          <button>
-            <h3>Mis Pagos</h3>
-          </button>
-          <button>
-            <h3>ChatBot</h3>
-          </button>
-          <button>
-            <h3>Reclamos</h3>
-          </button>
-          <button>
-            <h3>Preguntas Frecuentes</h3>
-          </button>
-          <button>
-            <h3>Salir</h3>
-          </button>
+          <TouchableOpacity onPress={toggleVisibility}>X</TouchableOpacity>
+          <TouchableOpacity>Mi Factura</TouchableOpacity>
+          <TouchableOpacity>Mis Pagos</TouchableOpacity>
+          <TouchableOpacity>ChatBot</TouchableOpacity>
+          <TouchableOpacity>Reclamos</TouchableOpacity>
+          <TouchableOpacity>Preguntas Frecuentes</TouchableOpacity>
+          <TouchableOpacity>Salir</TouchableOpacity>
         </>
       )}
-    </div>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  taskbar: {
+    width: 250,
+    height: '100%',
+    backgroundColor: '#333',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 1000,
+    flexDirection: 'column',
+    paddingTop: 60
+  },
+  taskbarButton: {
+    borderWidth: 0, // No hay border: none en React Native, se usa borderWidth: 0
+    backgroundColor: 'transparent', // 'none' no es válido, 'transparent' se usa en lugar de 'none'
+    color: 'white',
+    padding: 10,
+    textAlign: 'left',
+    fontSize: 16,
+    cursor: 'pointer' // No hay 'cursor' en React Native
+  }
+});
 
 export default TaskBar;
