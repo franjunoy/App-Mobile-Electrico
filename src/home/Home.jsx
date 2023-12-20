0a4c14ca1e36b0d39cb64f454cbc220f75346e69
franjunoy <franjunoy@hotmail.com>
2023-12-20 10:24:53 -0300

    home

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,27 +1,11 @@
 import { useState } from 'react';
 import Nav from '../nav/Nav';
-import TaskBar from '../taskbar/TaskBar';
-import { Link } from 'react-router-dom';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-web';
 
 const Home = () => {
-  const [isTaskBarVisible, setIsTaskBarVisible] = useState(false);
-
-  const toggleTaskBarVisibility = () => {
-    setIsTaskBarVisible(!isTaskBarVisible);
-  };
-
   return (
     <View>
-      <Nav toggleTaskBar={toggleTaskBarVisibility} />
-      <TaskBar
-        isVisible={isTaskBarVisible}
-        toggleVisibility={toggleTaskBarVisibility}
-      />
-      <Link to='/Calculator'>
-        <Button> title="Calculadora"</Button>
-      </Link>
+      <Nav />
     </View>
   );
 };
