8735bdd6d66e586366496a6cfed84fbda621ecce
franjunoy <franjunoy@hotmail.com>
2023-12-20 10:24:57 -0300

    logo

--- src/logo/Logo.jsx
+++ src/logo/Logo.jsx
@@ -1,15 +1,25 @@
-import LogoElectrico from '../assets/LogoElectrico.png';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 // eslint-disable-next-line react/prop-types
 const Logo = ({ toggleTaskBar }) => {
   return (
     <View>
-      <button onClick={toggleTaskBar}>
-        <img src={LogoElectrico} alt='Logo Electrico' className={style.logo} />
-      </button>
+      <TouchableOpacity onPress={toggleTaskBar}>
+        <Image
+          source={require('../assets/LogoElectrico.png')}
+          style={styles.image}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 20
+  }
+});
 
 export default Logo;
