77c40ad4b03ef6484963d3574d2e5de63443edad
franjunoy <franjunoy@hotmail.com>
2023-12-20 12:51:41 -0300

    Home

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
 import Nav from '../nav/Nav';
+import TaskBar from '../taskbar/TaskBar';
 import { StyleSheet, Text, View } from 'react-native';
 
 const Home = () => {
+  const [isTaskBarVisible, setIsTaskBarVisible] = useState(false);
+
+  const toggleTaskBarVisibility = () => {
+    setIsTaskBarVisible(!isTaskBarVisible);
+  };
   return (
     <View>
-      <Nav />
+      <Nav toggleTaskBar={toggleTaskBarVisibility} />
+      <TaskBar
+        isVisible={isTaskBarVisible}
+        toggleVisibility={toggleTaskBarVisibility}
+      />
     </View>
   );
 };
