4024909538f5ce1d30a0c7c9414dca39144e276f
franjunoy <franjunoy@hotmail.com>
2023-12-21 16:51:50 -0300

    Header

---
+++ src/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Header as HeaderRNE, Icon } from '@rneui/themed';
+import {
+  TouchableOpacity,
+  GestureHandlerRootView
+} from 'react-native-gesture-handler';
+
+const Header = (toggleMenu) => {
+  return (
+    <GestureHandlerRootView>
+      <HeaderRNE
+        leftComponent={
+          <View style={styles.menu}>
+            <TouchableOpacity onPress={toggleMenu}>
+              <Icon name='menu' color='white' size={30} />
+            </TouchableOpacity>
+          </View>
+        }
+        rightComponent={
+          <View style={styles.person}>
+            <TouchableOpacity>
+              <Icon name='person' color='white' size={30} />
+            </TouchableOpacity>
+          </View>
+        }
+        containerStyle={{ backgroundColor: '#FFE457', height: 60 }}
+      />
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Header;
