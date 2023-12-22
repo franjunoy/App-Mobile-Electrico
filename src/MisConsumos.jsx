05ba7be3fab0693d1c0b01fdc780c2d4c0af726b
franjunoy <franjunoy@hotmail.com>
2023-12-22 15:19:57 -0300

    MisConsumos

--- src/MisConsumos.jsx
+++ src/MisConsumos.jsx
@@ -1,48 +1,34 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { Button } from '@rneui/themed';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const MisConsumos = () => {
   return (
-    <>
-      <ScrollView>
-        <View style={styles.contentView}>
-          <View style={styles.buttonsContainer}>
-            <Button
-              buttonStyle={{
-                backgroundColor: '#FFE457',
-                borderRadius: 5,
-                height: 70
-              }}
-              containerStyle={{
-                width: 250,
-                marginHorizontal: 50,
-                marginVertical: 10
-              }}
-            >
-              <View>
-                <Icon name='plug' size={40} color='black' marginLeft={55} />
-                <Text style={styles.text}>Mis consumos</Text>
-              </View>
-              <Icon
-                name='chevron-right'
-                size={25}
-                color='black'
-                marginRight={10}
-              />
-            </Button>
-          </View>
+    <View style={styles.buttonsContainer}>
+      <Button
+        buttonStyle={{
+          backgroundColor: '#FFE457',
+          borderRadius: 5,
+          height: 70
+        }}
+        containerStyle={{
+          width: 250,
+          marginHorizontal: 50,
+          marginVertical: 10
+        }}
+      >
+        <View style={styles.view}>
+          <Icon name='plug' size={40} color='black' marginRight={10} />
+          <Text style={styles.text}>Mis consumos</Text>
         </View>
-      </ScrollView>
-    </>
+        <Icon name='chevron-right' size={25} color='black' marginLeft={25} />
+      </Button>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  contentView: {
-    flex: 1
-  },
   buttonsContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
@@ -51,9 +37,13 @@ const styles = StyleSheet.create({
     width: '100%',
     marginVertical: 20
   },
+  view: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
   text: {
-    marginLeft: 15,
-    marginRight: 80,
+    marginLeft: 0,
+    marginRight: 10,
     fontFamily: 'Helvetica-Bold',
     fontSize: 18
   }
