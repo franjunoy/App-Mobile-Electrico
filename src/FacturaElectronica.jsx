2f6fee0a064ee1b38d9dee23ccdcf1c77effab5a
franjunoy <franjunoy@hotmail.com>
2023-12-22 15:19:50 -0300

    FacturaElectronica

--- src/FacturaElectronica.jsx
+++ src/FacturaElectronica.jsx
@@ -1,40 +1,37 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { Button } from '@rneui/themed';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const FacturaElectronica = () => {
   return (
-    <>
-      <ScrollView>
-        <View style={styles.contentView}>
-          <View style={styles.buttonsContainer}>
-            <Button
-              buttonStyle={{
-                backgroundColor: '#FFE457',
-                borderRadius: 5,
-                height: 70
-              }}
-              containerStyle={{
-                width: 250,
-                marginHorizontal: 50,
-                marginVertical: 10
-              }}
-            >
-              <Icon name='receipt' size={30} color='black' />
-              <Text style={styles.textStyle}>Factura electronica</Text>
-            </Button>
+    <View style={styles.buttonsContainer}>
+      <Button
+        buttonStyle={{
+          backgroundColor: '#FFE457',
+          borderRadius: 5,
+          height: 70
+        }}
+        containerStyle={{
+          width: 250,
+          marginHorizontal: 50,
+          marginVertical: 10
+        }}
+      >
+        <View style={styles.view}>
+          <Icon name='receipt' size={40} color='black' marginRight={13} />
+          <View style={styles.viewText}>
+            <Text style={styles.text}>Factura </Text>
+            <Text style={styles.text}>electronica</Text>
           </View>
         </View>
-      </ScrollView>
-    </>
+        <Icon name='chevron-right' size={25} color='black' marginLeft={30} />
+      </Button>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  contentView: {
-    flex: 1
-  },
   buttonsContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
@@ -43,7 +40,21 @@ const styles = StyleSheet.create({
     width: '100%',
     marginVertical: 20
   },
-  textStyle: { marginLeft: 10 }
+  view: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  viewText: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginRight: 30
+  },
+  text: {
+    marginLeft: 0,
+
+    fontFamily: 'Helvetica-Bold',
+    fontSize: 18
+  }
 });
 
 export default FacturaElectronica;
