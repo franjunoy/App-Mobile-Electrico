4407fecb700e2cd5254238a829c04fae446469d3
franjunoy <franjunoy@hotmail.com>
2023-12-22 14:39:26 -0300

    HOme

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { View } from 'react-native';
 import Header from './Header';
+import MisConsumos from './MisConsumos';
+import FacturaElectronica from './FacturaElectronica';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +13,8 @@ const Home = () => {
   return (
     <View>
       <Header toggleMenu={toggleMenu} />
+      <MisConsumos />
+      <FacturaElectronica />
     </View>
     // <Menu isOpen={isOpen} toggleMenu={toggleMenu} />
   );
