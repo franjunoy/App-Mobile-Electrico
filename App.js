4efeb2291e68103433686fdd292f6c8edec86e25
franjunoy <franjunoy@hotmail.com>
2024-01-05 10:12:50 -0300

    App

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Home from './src/Home';
-import Menu from './src/Menu';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Menu />
+      <Home />
       <StatusBar style='auto' />
     </View>
   );
