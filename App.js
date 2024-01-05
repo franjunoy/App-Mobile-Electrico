41b9aa1116eb626a1b802538384b900f6398087e
franjunoy <franjunoy@hotmail.com>
2024-01-05 12:35:28 -0300

    App

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
 import Home from './src/Home';
+import Menu from './src/Menu';
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <View style={styles.container}>
-      <Home />
+      <View style={styles.layer}>
+        <Home toggleMenu={toggleMenu} />
+        {isOpen && (
+          <View style={StyleSheet.absoluteFill}>
+            <Menu isOpen={isOpen} toggleMenu={toggleMenu} />
+          </View>
+        )}
+      </View>
       <StatusBar style='auto' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { top: 25 }
+  container: {
+    flex: 1,
+    paddingTop: 25
+  },
+  layer: {
+    flex: 1
+  }
 });
