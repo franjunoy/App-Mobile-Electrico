8a620880b76ad3f9dc46b3f113535eabb8d314d0
franjunoy <franjunoy@hotmail.com>
2024-01-10 10:04:24 -0300

    App

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Route, Switch } from 'react-router-native';
 import Main from './src/Main';
+import MisConsumos from './src/src-interno/MisConsumos';
+import MapaDeCortes from './src/src-interno/MapaDeCortes';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Main />
+      <Switch>
+        <Route path='/' exact>
+          <Main />
+        </Route>
+        <Route path='/misconsumos' exact>
+          <MisConsumos />
+        </Route>
+        <Route path='/factura' exact>
+          <Text>Veras tu factura</Text>
+        </Route>
+        <Route path='/calculadora' exact>
+          <Text>Aca va la calculadora</Text>
+        </Route>
+        <Route path='/mapa' exact>
+          <MapaDeCortes />
+        </Route>
+        <Route path='/soporte' exact>
+          <Text>Ayuda o soporte</Text>
+        </Route>
+      </Switch>
+
       <StatusBar style='auto' />
     </View>
   );
