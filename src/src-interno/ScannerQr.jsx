c4094c8cc046fea867fdc5f90660f1f3441d772e
franjunoy <franjunoy@hotmail.com>
2024-01-17 14:21:08 -0300

    ScannerQr

---
+++ src/src-interno/ScannerQr.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+const ScannerQr = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setData(data);
+    alert(`Código ${type}: ${data}`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Solicitando permiso para usar la cámara...</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>Acceso a la cámara denegado</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <Button title={'Escanear de nuevo'} onPress={() => setScanned(false)} />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end'
+  }
+});
+
+export default ScannerQr;
