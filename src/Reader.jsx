c3a19d7abcd88bcf677c8457c91b7e2df9cf1b9e
franjunoy <franjunoy@hotmail.com>
2024-01-17 14:20:42 -0300

    Reader

---
+++ src/Reader.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Button } from '@rneui/themed';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { useNavigation } from '@react-navigation/native';
+
+const Reader = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Button
+        color='transparent'
+        onPress={() => navigation.navigate('ScannerQr')}
+      >
+        <Icon name='desktop' size={25} color='black' />
+      </Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%'
+  },
+
+  text: {
+    marginRight: 250,
+    fontFamily: 'Helvetica-Bold',
+    fontSize: 20
+  }
+});
+
+export default Reader;
