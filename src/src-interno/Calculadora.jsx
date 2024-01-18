4520dc4e87874a779f2ba4c5ded9ad251797e15e
franjunoy <franjunoy@hotmail.com>
2024-01-18 10:25:15 -0300

    Calculadora

--- src/src-interno/Calculadora.jsx
+++ src/src-interno/Calculadora.jsx
@@ -1,15 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
 
 const Calculadora = () => {
+  const buttonsData = [
+    'Botón 1',
+    'Botón 2',
+    'Botón 3',
+    'Botón 4',
+    'Botón 5',
+    'Botón 6',
+    'Botón 7',
+    'Botón 8',
+    'Botón 9',
+    'Botón 10',
+    'Botón 11',
+    'Botón 12'
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Calculadora</Text>
-      <StatusBar style='auto' />
+      {buttonsData.map((buttonText, index) => (
+        <TouchableOpacity
+          key={index}
+          style={styles.button}
+          onPress={() => onPressButton(buttonText)}
+        >
+          <Text style={styles.buttonText}>{buttonText}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: 10
+  },
+  button: {
+    width: '30%',
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3498db',
+    marginVertical: 10,
+    borderRadius: 8
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16
+  }
+});
 
 export default Calculadora;
