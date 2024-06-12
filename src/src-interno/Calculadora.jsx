d08c61fd434cc1575bce3dbfa516ab5db0843717
franjunoy <franjunoy@hotmail.com>
2024-06-12 18:11:54 -0300

    Calculadora

--- src/src-interno/Calculadora.jsx
+++ src/src-interno/Calculadora.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const Calculadora = () => {
