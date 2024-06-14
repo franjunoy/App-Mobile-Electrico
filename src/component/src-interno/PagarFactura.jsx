c12215b19289328877b2c1ccca298d0914aa274a
franjunoy <franjunoy@hotmail.com>
2024-06-14 20:07:26 -0300

    Facturas

---
+++ src/component/src-interno/PagarFactura.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text } from 'react-native';
+
+const PagarFactura = () => {
+  return (
+    <View>
+      <Text>PagarFactura</Text>
+    </View>
+  );
+};
+
+export default PagarFactura;
