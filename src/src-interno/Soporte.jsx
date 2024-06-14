d15a5b0208ea6ecabf0f3747b59175eab4249c9a
franjunoy <franjunoy@hotmail.com>
2024-06-13 21:04:24 -0300

    soporte

--- src/src-interno/Soporte.jsx
+++ src/src-interno/Soporte.jsx
@@ -3,13 +3,11 @@ import { StyleSheet, View, Text } from 'react-native';
 
 const Soporte = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Soporte</Text>
       <StatusBar style='auto' />
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default Soporte;
