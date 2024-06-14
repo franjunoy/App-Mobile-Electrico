5dd9ad6f130048ea9dd6843ee303fcde8df06b02
franjunoy <franjunoy@hotmail.com>
2024-06-14 20:06:40 -0300

    menu

---
+++ src/component/Menu.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, StyleSheet, FlatList, Image, Text } from 'react-native';
+import { ListItem, Button } from '@rneui/themed';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {
+  TouchableOpacity,
+  GestureHandlerRootView
+} from 'react-native-gesture-handler';
+
+const Menu = ({ isOpen, toggleMenu }) => {
+  const menuItems = [
+    { icon: 'home', text: 'Inicio' },
+    { icon: 'money-bill', text: 'Mis Pagos' },
+    { icon: 'file-alt', text: 'Factuira Electronica' },
+    { icon: 'book-open', text: 'Preguntas Frecuentes' },
+    { icon: 'info-circle', text: 'Informacion' },
+    { icon: 'cog', text: 'Opciones' }
+  ];
+
+  const renderMenuItem = ({ item }) => (
+    <ListItem>
+      <Icon name={item.icon} color='black' size={25} />
+      <TouchableOpacity>
+        <Text>{item.text}</Text>
+      </TouchableOpacity>
+    </ListItem>
+  );
+
+  return (
+    <GestureHandlerRootView>
+      {isOpen && (
+        <View>
+          <View>
+            <View>
+              <Text>Cooperativa Eléctrica Pergamino</Text>
+            </View>
+            <TouchableOpacity onPress={toggleMenu}>
+              <View>
+                <Text>X</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <FlatList data={menuItems} renderItem={renderMenuItem}></FlatList>
+          <View></View>
+          <View>
+            <Icon name='home' color='black' size={30} marginLeft={10} />
+            <TouchableOpacity>
+              <Text>Salir</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </GestureHandlerRootView>
+  );
+};
+
+export default Menu;
