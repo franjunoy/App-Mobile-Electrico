64468bd14365c8f8371425405321ffc9e4bf4a28
franjunoy <franjunoy@hotmail.com>
2024-06-14 20:06:15 -0300

    main

---
+++ src/component/Main.jsx
@@ -0,0 +1,27 @@
+import { View } from 'react-native';
+import { useState } from 'react';
+import Home from './Home';
+import Menu from './Menu';
+
+const Main = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <View>
+      <View>
+        <Home toggleMenu={toggleMenu} />
+        {isOpen && (
+          <View className='absolute top-16 w-64 '>
+            <Menu isOpen={isOpen} toggleMenu={toggleMenu} />
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Main;
