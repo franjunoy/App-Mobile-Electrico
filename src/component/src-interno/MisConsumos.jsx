8415d43a0014aa11bb11743d90cc62056d7d41e1
franjunoy <franjunoy@hotmail.com>
2024-06-14 20:07:14 -0300

    Mis consumos

---
+++ src/component/src-interno/MisConsumos.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text } from 'react-native';
+
+const MisConsumos = () => {
+  return (
+    <View>
+      <Text>Cuanto llevas consumiendo</Text>
+    </View>
+  );
+};
+
+export default MisConsumos;
