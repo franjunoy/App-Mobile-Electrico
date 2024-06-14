b79ddac5502620963821c9bf5295a68b66fb354c
franjunoy <franjunoy@hotmail.com>
2024-06-13 21:02:51 -0300

    mis consumos

--- src/MisConsumos.jsx
+++ src/MisConsumos.jsx
@@ -8,46 +8,16 @@ const MisConsumos = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.buttonsContainer}>
-      <Button
-        buttonStyle={{
-          backgroundColor: '#FFEB81',
-          borderRadius: 10,
-          borderWidth: 1,
-          borderColor: '#d9d9d9',
-          height: 80,
-          width: 305,
-          marginHorizontal: 50
-        }}
-        onPress={() => navigation.navigate('MisConsumos')}
-      >
-        <View style={styles.view}>
+    <View>
+      <Button onPress={() => navigation.navigate('MisConsumos')}>
+        <View>
           <Icon name='plug' size={40} color='black' marginRight={20} />
-          <Text style={styles.text}>Mis consumos</Text>
+          <Text>Mis consumos</Text>
         </View>
         <Icon name='chevron-right' size={25} color='black' marginLeft={40} />
       </Button>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  buttonsContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    marginVertical: 10
-  },
-  view: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  text: {
-    marginRight: 10,
-    fontSize: 18
-  }
-});
 
 export default MisConsumos;
