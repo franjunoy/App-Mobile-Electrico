1c1b553918fab3662cec7feb564e4b796f6f7cc2
franjunoy <franjunoy@hotmail.com>
2024-06-14 20:06:05 -0300

    Navigatios

---
+++ src/Navigations.js
@@ -0,0 +1,31 @@
+import Main from './component/Main';
+import MisConsumos from './component/src-interno/MisConsumos';
+import MapaDeCortes from './component/src-interno/MapaDeCortes';
+import Calculadora from './component/src-interno/Calculadora';
+import PagarFactura from './component/src-interno/PagarFactura';
+import ScannerQr from './component/src-interno/ScannerQr';
+import Soporte from './component/src-interno/Soporte';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const Navigator = () => {
+  return (
+    <Stack.Navigator initialRouteName='Main'>
+      <Stack.Screen
+        name='Main'
+        component={Main}
+        options={{
+          headerShown: false
+        }}
+      />
+      <Stack.Screen name='MisConsumos' component={MisConsumos} />
+      <Stack.Screen name='MapaDeCortes' component={MapaDeCortes} />
+      <Stack.Screen name='ScannerQr' component={ScannerQr} />
+      <Stack.Screen name='PagarFactura' component={PagarFactura} />
+      <Stack.Screen name='Calculadora' component={Calculadora} />
+      <Stack.Screen name='Soporte' component={Soporte} />
+    </Stack.Navigator>
+  );
+};
+export default Navigator;
