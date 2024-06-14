5a7ed118c63d9e33ad69fdd9fcccc3e7c79e2022
franjunoy <franjunoy@hotmail.com>
2024-06-14 20:05:20 -0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'electric-yellow': '#FFFF33'
+      }
+    }
   },
   plugins: []
 };
