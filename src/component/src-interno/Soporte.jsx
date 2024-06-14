aa14777ae472ee6609f2f503d3898b2f0aceebde
franjunoy <franjunoy@hotmail.com>
2024-06-14 20:07:31 -0300

    Soporte

---
+++ src/component/src-interno/Soporte.jsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Soporte = () => {
+  return (
+    <View>
+      <Text>Soporte</Text>
+      <StatusBar style='auto' />
+    </View>
+  );
+};
+
+export default Soporte;
