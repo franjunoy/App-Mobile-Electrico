b776b1ebb91d529d00b55e1718f15b1179f5d040
franjunoy <franjunoy@hotmail.com>
2024-06-15 02:36:34 -0300

    Header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { View, Image, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { Icon } from '@rneui/themed';
+import Menu from '../../assets/Menu.png';
+import Persona from '../../assets/Persona.png';
 import Logo from '../../assets/Logo.png';
 
 const Header = ({ toggleMenu }) => {
   return (
-    <View className='w-full h-16 bg-electric-yellow items-center flex-row '>
-      <View className='flex-row left-2 items-center'>
-        <TouchableOpacity onPress={toggleMenu} className=''>
-          <Image source={Logo} className=' w-10 h-10' />
+    <View className='w-full h-16 bg-electric-yellow items-center flex-row justify-between px-1'>
+      <View className='flex-row '>
+        <TouchableOpacity onPress={toggleMenu}>
+          <View>
+            <Image source={Menu} className='w-16 h-16' />
+          </View>
         </TouchableOpacity>
-        <Image source={Logo} className=' w-10 h-10 left-2' />
+        <View>
+          <Image source={Logo} className='w-16 h-16' />
+        </View>
       </View>
-      <View className='left-80'>
+      <View>
         <TouchableOpacity>
-          <Icon name='person' color='black' size={35} alignItems='center' />
+          <Image source={Persona} className='w-16 h-16' />
         </TouchableOpacity>
       </View>
       <StatusBar style='auto' />
