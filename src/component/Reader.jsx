8f02430ade6a8e36b8c8818c3589f58da2fca2f3
franjunoy <franjunoy@hotmail.com>
2024-06-15 02:36:57 -0300

    reader

--- src/component/Reader.jsx
+++ src/component/Reader.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Button } from '@rneui/themed';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import { View, Image, TouchableOpacity } from 'react-native';
+import Scanner from '../../assets/Scanner.png';
 import { useNavigation } from '@react-navigation/native';
 
 const Reader = () => {
   const navigation = useNavigation();
 
   return (
-    <View>
-      <Button onPress={() => navigation.navigate('ScannerQr')}>
-        <Icon name='desktop' size={25} color='black' />
-      </Button>
+    <View className='items-center bg-gray-200'>
+      <View className='border p-2 bg-electric-yellow rounded-lg'>
+        <TouchableOpacity onPress={() => navigation.navigate('ScannerQr')}>
+          <Image source={Scanner} className='w-20 h-20' />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
