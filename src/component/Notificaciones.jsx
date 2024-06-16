b65bfe4cde2c434a66e10b09b2b1a5c39515748c
franjunoy <franjunoy@hotmail.com>
2024-06-16 16:01:26 -0300

    Notificaciones

--- src/component/Notificaciones.jsx
+++ src/component/Notificaciones.jsx
@@ -4,7 +4,7 @@ import Notificacion from '../../assets/Notificacion.png';
 
 const Notificaciones = () => {
   return (
-    <View className='w-full h-14 items-center bg-gray-200 flex-row justify-between px-5'>
+    <View className='w-full h-14 items-center flex-row justify-between px-5'>
       <View className=' '>
         <Text className=' text-xl text-electric-yellow font-bold'>¡Hola!</Text>
       </View>
