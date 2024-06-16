dbab7153b7f621bbbfe59dd9fd7e601004c7fea7
franjunoy <franjunoy@hotmail.com>
2024-06-16 16:01:49 -0300

    Soporte

--- src/component/Soporte.jsx
+++ src/component/Soporte.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Button } from '@rneui/themed';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import Soporte from '../../assets/Soporte.png';
 import { useNavigation } from '@react-navigation/native';
 
 const FacturaElectronica = () => {
@@ -9,24 +8,21 @@ const FacturaElectronica = () => {
 
   return (
     <View>
-      <Button onPress={() => navigation.navigate('Soporte')}>
-        <View flexDirection='colum'>
-          <View>
-            <Icon
-              name='headset'
-              size={20}
-              color='black'
-              padding={5}
-              justifyContent='center'
-              alignItems='center'
-            />
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Soporte')}
+        className='w-28 h-24 border p-2 bg-electric-yellow rounded-lg'
+      >
+        <View>
+          <View className='items-center'>
+            <Image source={Soporte} className='w-10 h-10' />
           </View>
-          <View>
-            <Text>Soporte</Text>
-            <Text>Tecnico</Text>
+          <View className='mt-2'>
+            <Text className='text-xs font-bold text-center'>
+              Soporte Tecnico
+            </Text>
           </View>
         </View>
-      </Button>
+      </TouchableOpacity>
     </View>
   );
 };
