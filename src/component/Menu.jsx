d98ac4a5ae47b1d9df0370bc4ac55a3b8755d231
franjunoy <franjunoy@hotmail.com>
2024-06-17 19:51:42 -0300

    Menu

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,52 +1,80 @@
 import React from 'react';
-import { View, StyleSheet, FlatList, Image, Text } from 'react-native';
-import { ListItem, Button } from '@rneui/themed';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import {
-  TouchableOpacity,
-  GestureHandlerRootView
-} from 'react-native-gesture-handler';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Apagar from '../../assets/Apagar.png';
+import Home from '../../assets/Home.png';
+import Pagos from '../../assets/Pagos.png';
+import Factura from '../../assets/Factura.png';
+import Preguntas from '../../assets/Preguntas.png';
+import Informacion from '../../assets/Informacion.png';
+import Configuracion from '../../assets/Configuracion.png';
 
 const Menu = ({ isOpen, toggleMenu }) => {
-  const menuItems = [
-    { icon: 'home', text: 'Inicio' },
-    { icon: 'money-bill', text: 'Mis Pagos' },
-    { icon: 'file-alt', text: 'Factuira Electronica' },
-    { icon: 'book-open', text: 'Preguntas Frecuentes' },
-    { icon: 'info-circle', text: 'Informacion' },
-    { icon: 'cog', text: 'Opciones' }
-  ];
-
-  const renderMenuItem = ({ item }) => (
-    <ListItem>
-      <Icon name={item.icon} color='black' size={25} />
-      <TouchableOpacity>
-        <Text>{item.text}</Text>
-      </TouchableOpacity>
-    </ListItem>
-  );
-
   return (
     <GestureHandlerRootView>
       {isOpen && (
         <View>
-          <View>
-            <View>
-              <Text>Cooperativa Eléctrica</Text>
+          <View className='flex-row h-16 justify-between border-b-2 p-2 bg-electric-yellow items-center'>
+            <View className='items-start'>
+              <Text className='text-lg font-bold'>Cooperativa Eléctrica</Text>
             </View>
-            <TouchableOpacity onPress={toggleMenu}>
-              <View>
-                <Text>X</Text>
-              </View>
-            </TouchableOpacity>
+            <View className=''>
+              <TouchableOpacity onPress={toggleMenu}>
+                <Text className='text-xl font-bold'>X</Text>
+              </TouchableOpacity>
+            </View>
           </View>
 
-          <View></View>
-          <View>
-            <Icon name='home' color='black' size={30} marginLeft={10} />
-            <TouchableOpacity>
-              <Text>Salir</Text>
-            </TouchableOpacity>
+          <View className='mt-4'>
+            <View>
+              <TouchableOpacity className='flex-row items-center justify-between p-1'>
+                <Text className='text-xl font-bold'>Home</Text>
+                <Image source={Home} className='w-8 w h-8' />
+              </TouchableOpacity>
+            </View>
+            <View className='mt-4'>
+              <TouchableOpacity className='flex-row items-center justify-between p-1'>
+                <Text className='text-xl font-bold'>Mis Pagos</Text>
+                <Image source={Pagos} className='w-9 w h-9' />
+              </TouchableOpacity>
+            </View>
+            <View className='mt-4'>
+              <TouchableOpacity className='flex-row items-center justify-between p-1'>
+                <Text className='text-xl font-bold'>Factuira Electronica</Text>
+                <Image source={Factura} className='w-10 w h-10' />
+              </TouchableOpacity>
+            </View>
+            <View className='mt-4'>
+              <TouchableOpacity className='flex-row items-center justify-between p-1'>
+                <Text className='text-xl font-bold'>Preguntas frecuentes</Text>
+                <Image source={Preguntas} className='w-9 w h-9' />
+              </TouchableOpacity>
+            </View>
+            <View className='mt-4'>
+              <TouchableOpacity className='flex-row items-center justify-between p-1'>
+                <Text className='text-xl font-bold'>Informacion</Text>
+                <Image source={Informacion} className='w-9 w h-9' />
+              </TouchableOpacity>
+            </View>
+            <View className='mt-4'>
+              <TouchableOpacity className='flex-row items-center justify-between p-1'>
+                <Text className='text-xl font-bold'>Configuraciones</Text>
+                <Image source={Configuracion} className='w-9 w h-9' />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <View className='mt-11'></View>
+
+          <View className='flex-row h-16 justify-between p-2 border-y-2 bg-electric-yellow items-center mt-80'>
+            <View>
+              <Text className='text-lg font-bold'>Salir</Text>
+            </View>
+            <View>
+              <TouchableOpacity>
+                <Image source={Apagar} className='w-10 w h-10' />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       )}
