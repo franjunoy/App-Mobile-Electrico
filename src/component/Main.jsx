8a85fe3d304569c619b56f70a23ec2d01afa6121
franjunoy <franjunoy@hotmail.com>
2024-06-17 21:07:13 -0300

    Main

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,25 +1,42 @@
-import { View } from 'react-native';
-import { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Animated, TouchableOpacity } from 'react-native';
 import Home from './Home';
 import Menu from './Menu';
 
 const Main = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const animation = useRef(new Animated.Value(0)).current;
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: isOpen ? 1 : 0,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  }, [isOpen]);
+
+  const menuStyle = {
+    transform: [
+      {
+        translateX: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [-1000, 0]
+        })
+      }
+    ],
+    opacity: animation
+  };
+
   return (
-    <View>
-      <View>
-        <Home toggleMenu={toggleMenu} />
-        {isOpen && (
-          <View className='absolute top-10 h-full w-full '>
-            <Menu isOpen={isOpen} toggleMenu={toggleMenu} />
-          </View>
-        )}
-      </View>
+    <View className='flex-1'>
+      <Home toggleMenu={toggleMenu} />
+      <Animated.View className='absolute top-10' style={[menuStyle]}>
+        <Menu isOpen={isOpen} toggleMenu={toggleMenu} />
+      </Animated.View>
     </View>
   );
 };
