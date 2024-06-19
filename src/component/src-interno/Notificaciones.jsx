2cd78982915c7bf6c93036b2f8a842b2334f1304
franjunoy <franjunoy@hotmail.com>
2024-06-19 18:56:43 -0300

    Notificaciones

---
+++ src/component/src-interno/Notificaciones.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+const Notificaciones = () => {
+  return (
+    <View className=' '>
+      <Text className=' text-xl font-bold'>Estas son tus notificaciones</Text>
+    </View>
+  );
+};
+
+export default Notificaciones;
