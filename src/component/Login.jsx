00539cbe263f0202f009ff978d2fb56a3a378eea
franjunoy <franjunoy@hotmail.com>
2024-06-22 17:33:45 -0300

    Login

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -27,79 +27,81 @@ const LoginComponent = () => {
   };
 
   return (
-    <View className='flex flex-col items-center'>
-      <View className='flex flex-row items-center w-full'>
-        <View className='flex-1 mr-14'>
-          <Text className='text-center text-zinc-900 text-xl font-bold mt-3'>
-            Iniciar sesión
-          </Text>
-        </View>
+    <View className='flex-1 flex-col mt-10 w-full h-full bg-gray-200'>
+      <View className=''>
+        <Text className='text-center text-black text-2xl font-bold mt-3'>
+          Iniciar sesión
+        </Text>
       </View>
+
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <View className='flex-1 justify-center items-center'>
-          <View className='w-[358px] h-full items-center mt-2'>
-            <View className='h-[40px] justify-center items-start'>
-              <Text className='text-center text-teal-900 text-2xl font-bold'>
-                Bienvenido de nuevo! 👋
-              </Text>
-            </View>
-            <View className='h-[65px] justify-center items-start'>
-              <Text className='text-gray-500 text-sm'>
-                Inicia sesión con tu cuenta
-              </Text>
-            </View>
-            <View>
-              <Text className='text-teal-900 text-base'>Email</Text>
-              <TextInput
-                className='w-[320px] mt-2 h-[40px] border border-teal-900 rounded-lg px-2'
-                placeholder='Ingresa tu email'
-                value={email}
-                onChangeText={setEmail}
-                keyboardType='email-address'
-                autoCapitalize='none'
-              />
-            </View>
-            <View className='mt-6'>
-              <Text className='text-teal-900 text-base'>Contraseña</Text>
-              <TextInput
-                className='w-[320px] mt-2 h-[40px] border border-teal-900 rounded-lg px-2'
-                placeholder='Ingresa tu contraseña'
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-              />
-              <View className='w-[320px] h-[20px] flex flex-row items-center mt-4'>
-                <TouchableOpacity onPress={toggleCheckbox}>
-                  <Icon
-                    name={isChecked ? 'check-box' : 'check-box-outline-blank'}
-                    size={20}
-                    color='#6C6C6C'
-                  />
-                </TouchableOpacity>
-                <Text className='text-[#6C6C6C] text-xs ml-2'>Recordarme</Text>
-                <TouchableOpacity>
-                  <Text className='text-[#6C6C6C] text-xs ml-14'>
-                    ¿Has olvidado la contraseña?
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-            <View className=' w-52 h-12 mt-12 justify-center items-center border rounded-lg bg-electric-yellow'>
-              <TouchableOpacity onPress={navigateToHome}>
-                <Text className='text-xl font-bold'>Ingresar</Text>
+        <View className='items-center mt-20'>
+          <View className=''>
+            <Text className='text-center text-black text-2xl font-bold'>
+              Bienvenido de nuevo! 👋
+            </Text>
+          </View>
+          <View className='mt-3'>
+            <Text className='text-gray-700 text-sm font-medium'>
+              Inicia sesión con tu cuenta
+            </Text>
+          </View>
+          <View className='mt-10'>
+            <Text className='text-black text-base font-semibold'>Email</Text>
+            <TextInput
+              className='w-80 h-10 mt-2 border border-black rounded-lg px-2 font-semibold'
+              placeholder='Ingresa tu email'
+              value={email}
+              onChangeText={setEmail}
+              keyboardType='email-address'
+              autoCapitalize='none'
+            />
+          </View>
+          <View className='mt-6'>
+            <Text className='text-black text-base font-semibold'>
+              Contraseña
+            </Text>
+            <TextInput
+              className='w-80 h-10 mt-2 border border-black rounded-lg px-2 font-semibold'
+              placeholder='Ingresa tu contraseña'
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+            />
+            <View className='w-[320px] h-[20px] flex flex-row items-center mt-4'>
+              <TouchableOpacity onPress={toggleCheckbox}>
+                <Icon
+                  name={isChecked ? 'check-box' : 'check-box-outline-blank'}
+                  size={20}
+                  color='#6C6C6C'
+                />
               </TouchableOpacity>
-            </View>
-            <View className='w-[270px] h-[22px] mt-2 flex flex-row justify-center items-center'>
-              <Text className='text-[#6C6C6C] text-xs'>
-                ¿Aún no tienes cuenta?
+              <Text className='text-[#6C6C6C] text-xs font-semibold ml-1'>
+                Recordarme
               </Text>
-              <TouchableOpacity /* onPress={navigateToRegister} */>
-                <Text className='border-teal-900 text-xs underline ml-1'>
-                  Regístrate
+              <TouchableOpacity>
+                <Text className='text-[#6C6C6C] text-xs font-semibold ml-16'>
+                  ¿Has olvidado la contraseña?
                 </Text>
               </TouchableOpacity>
             </View>
-            {/* <View className='mt-16 flex flex-col items-center'>
+          </View>
+          <View className=' w-52 h-12 mt-12 justify-center items-center border rounded-lg bg-electric-yellow'>
+            <TouchableOpacity onPress={navigateToHome}>
+              <Text className='text-xl font-bold'>Ingresar</Text>
+            </TouchableOpacity>
+          </View>
+          <View className='w-[270px] h-[22px] mt-2 flex flex-row justify-center items-center'>
+            <Text className='text-[#6C6C6C] text-xs font-semibold'>
+              ¿Aún no tienes cuenta?
+            </Text>
+            <TouchableOpacity /* onPress={navigateToRegister} */>
+              <Text className='border-teal-900 text-xs font-semibold underline ml-1'>
+                Regístrate
+              </Text>
+            </TouchableOpacity>
+          </View>
+          {/* <View className='mt-16 flex flex-col items-center'>
               <Text className='text-[#6C6C6C] text-xs mb-2'>
                 O inicia sesión con
               </Text>
@@ -115,8 +117,7 @@ const LoginComponent = () => {
                 </TouchableOpacity>
               </View>
             </View> */}
-            <View className='h-[150px]'></View>
-          </View>
+          <View className='h-[150px]'></View>
         </View>
       </ScrollView>
     </View>
