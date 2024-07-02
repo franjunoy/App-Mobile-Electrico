02407d53dbd6cc0755809bf4e9ac3e7614fa486c
franjunoy <franjunoy@hotmail.com>
2024-07-02 14:40:46 -0300

    Notificaciones

--- src/component/src-interno/Notificaciones.jsx
+++ src/component/src-interno/Notificaciones.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 const Notificaciones = () => {
+  const route = useRoute();
+  const { user } = route.params;
   return (
-    <View className=' '>
-      <Text className=' text-xl font-bold'>Estas son tus notificaciones</Text>
+    <View className='mt-3 items-center justify-center'>
+      <ScrollView>
+        {user.notificaciones
+          ?.slice()
+          .reverse()
+          .map((notificacion, index) => (
+            <View
+              key={index}
+              className='border rounded-lg h-14 w-96 items-center justify-center mb-3'
+            >
+              <Text className='text-lg font-bold'>{notificacion.mensaje}</Text>
+            </View>
+          ))}
+      </ScrollView>
     </View>
   );
 };
