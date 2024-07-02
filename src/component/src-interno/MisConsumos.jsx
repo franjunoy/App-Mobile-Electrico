e89a50d9a9a8585ceb59ddc1ff6a608fe85670ef
franjunoy <franjunoy@hotmail.com>
2024-07-01 22:16:29 -0300

    COnsumos

--- src/component/src-interno/MisConsumos.jsx
+++ src/component/src-interno/MisConsumos.jsx
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 const MisConsumos = () => {
+  const route = useRoute();
+  const { user } = route.params;
   return (
-    <View>
-      <Text>Cuanto llevas consumiendo</Text>
+    <View className='mt-3 items-center justify-center'>
+      <ScrollView>
+        {user.user.misConsumos
+          ?.slice()
+          .reverse()
+          .map((consumos, index) => (
+            <View
+              key={index}
+              className='border rounded-lg h-32 w-56 items-center justify-center mb-3'
+            >
+              <Text className='text-lg font-bold'>{consumos.descripcion}</Text>
+              <Text className='text-base font-semibold'>
+                {consumos.consumo}KWh
+              </Text>
+            </View>
+          ))}
+      </ScrollView>
     </View>
   );
 };
