1d929851fe8cb5c4515f057783250deb01bc361d
franjunoy <franjunoy@hotmail.com>
2024-07-01 21:17:30 -0300

    Factura

--- src/component/src-interno/PagarFactura.jsx
+++ src/component/src-interno/PagarFactura.jsx
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 const PagarFactura = () => {
+  const route = useRoute();
+  const { user } = route.params;
+
   return (
-    <View>
-      <Text>PagarFactura</Text>
+    <View className='mt-3 items-center justify-center'>
+      <ScrollView>
+        {user.user.facturasElectronicas
+          ?.slice()
+          .reverse()
+          .map((factura, index) => (
+            <View
+              key={index}
+              className='border rounded-lg h-32 w-56 items-center justify-center mb-3'
+            >
+              <Text className='text-lg font-bold'>{factura.descripcion}</Text>
+              <Text className='text-base font-semibold'>${factura.monto}</Text>
+            </View>
+          ))}
+      </ScrollView>
     </View>
   );
 };
