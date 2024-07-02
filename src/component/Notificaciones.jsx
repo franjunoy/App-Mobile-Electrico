17d4f359acdfa8b66e6b0a080cdcac6b5ffa82da
franjunoy <franjunoy@hotmail.com>
2024-07-02 14:40:37 -0300

    Notificaciones

--- src/component/Notificaciones.jsx
+++ src/component/Notificaciones.jsx
@@ -3,15 +3,18 @@ import { View, Text, Image, TouchableOpacity } from 'react-native';
 import Notificacion from '../../assets/Notificacion.png';
 import { useNavigation } from '@react-navigation/native';
 
-const Notificaciones = () => {
+const Notificaciones = ({ user }) => {
   const navigation = useNavigation();
+  const navigateToNotificaciones = () => {
+    navigation.navigate('Notificaciones', { user: user });
+  };
   return (
     <View className='w-full h-14 items-center flex-row justify-between px-5'>
       <View className=' '>
         <Text className=' text-xl font-bold'>¡Hola!</Text>
       </View>
       <View>
-        <TouchableOpacity onPress={() => navigation.navigate('Notificaciones')}>
+        <TouchableOpacity onPress={navigateToNotificaciones}>
           <Image source={Notificacion} className='w-14 h-14' />
         </TouchableOpacity>
       </View>
